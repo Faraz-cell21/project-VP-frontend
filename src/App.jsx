ce11e42b96fdecf16c808fff12b5deba7b27051e
Faraz-cell21 <faraz127200@outlook.com>
2025-06-05 08:09:16 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-white">Working on Frontend of Project Vape with vite + react</h1>
+    </>
+  )
+}
+
+export default App
