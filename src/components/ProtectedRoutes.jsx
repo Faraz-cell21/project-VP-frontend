f6b3ec138fafa2ed4da554a76d211887f1e6b136
Faraz-cell21 <faraz127200@outlook.com>
2025-06-10 22:00:22 +0500

    Protected Routes

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../context/AuthContext";
 function ProtectedRoutes({ children, allowedRoles = [] }){
     const { user, loading } = useContext(AuthContext)
     if(loading){
-        return <h1>Loading...</h1>
+        return <h1>Loading....</h1>
     }
 
     if(!user){
